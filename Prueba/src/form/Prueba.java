efd4d824cb7d82db8390bdb38814233fa140c509
rtezen20 <i201914175.cibertec.edu.pe>
2019-05-06 18:29:44 -0500

    Creación de formulario ventas(botones, caja de texto)

--- Prueba/src/form/Prueba.java
+++ Prueba/src/form/Prueba.java
@@ -6,10 +6,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class Prueba extends JFrame {
+public class Prueba extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
+	private JLabel lblNewLabel;
+	private JTextField textField;
+	private JTextArea textArea;
+	private JButton btnNewButton;
 
 	/**
 	 * Launch the application.
@@ -35,8 +45,36 @@ public class Prueba extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(27, 30, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(119, 27, 86, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textArea = new JTextArea();
+		textArea.setBounds(27, 72, 298, 143);
+		contentPane.add(textArea);
+		
+		btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(this);
+		btnNewButton.setBounds(236, 26, 89, 23);
+		contentPane.add(btnNewButton);
 	}
-
+	public void actionPerformed(ActionEvent arg0) {
+		if (arg0.getSource() == btnNewButton) {
+			actionPerformedBtnNewButton(arg0);
+		}
+	}
+	protected void actionPerformedBtnNewButton(ActionEvent arg0) {
+		int cant=Integer.parseInt(textField.getText());
+		
+		
+		
+	}
 }
